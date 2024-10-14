acf9d2a79cee7e07f193f1f952a1fb1fddef1b01
thehawkone <thehawkone@list.ru>
2024-10-15 02:50:54 +0500

    configure program.cs

--- Users.API/Program.cs
+++ Users.API/Program.cs
@@ -1,9 +1,49 @@
+using System.Text;
+using Users.DataAccess;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Users.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<UsersDbContext>(
+    options => {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(UsersDbContext)));
+    });
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"])) 
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+//Add services to the container.
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapGet("/security/getMessage",
+    () => "Hello World!").RequireAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -12,7 +52,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.UseEndpoints(endpoints =>
+    endpoints.MapControllers());
 
 app.Run();
--- Users.API/obj/Debug/net9.0/Users.API.AssemblyInfo.cs
+++ Users.API/obj/Debug/net9.0/Users.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Users.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4e6c48372539f871ece466aa457acce73fe17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc403a4d7856c4b21760596b3ca49fbf036a3393")]
 [assembly: System.Reflection.AssemblyProductAttribute("Users.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Users.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
